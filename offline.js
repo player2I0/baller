233164efeb48efbe44e2dc82d82905d88aa9707b
player2I0 <59975613+player2I0@users.noreply.github.com>
2021-09-17 20:40:10 +0300

    inital

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1631900340,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ball-sheet0.png",
+		"images/cursor-sheet0.png",
+		"images/curtrail-sheet0.png",
+		"images/bound-sheet0.png",
+		"images/scrollm-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
